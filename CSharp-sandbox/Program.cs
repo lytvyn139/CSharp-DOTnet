8023aebb564a6c0d5abe2d8c85595f20e58548ba
IURII <lytvyn.139@gmail.com>
2020-08-03 21:56:49 -0500

    basic 13

--- CSharp-sandbox/Program.cs
+++ CSharp-sandbox/Program.cs
@@ -1,105 +1,40 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-namespace CSharp_sandbox
+
+namespace boxing
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // HoldTenNumbers();
-            // FourNamesArray();
-            // TenBoolArray();
-            userinfo(Icecream(), FourNamesArray());
-        }
-        /*             
-          ######### Three Basic Arrays ##########
-      */
-        //Create an array to hold integer values 0 through 9
-        static void HoldTenNumbers()
-        {
-
-            int num = 0;
-            int[] array = new int[10];
-            for (int i = 0; i < array.Length; i++)
+            //Create an empty List of type object
+            List<object> mylist = new List<object>();
+            Console.WriteLine(String.Join(", ", mylist));
+            //Add the following values to the list: 7, 28, -1, true, "chair"
+            mylist.Add(7);
+            mylist.Add(28);
+            mylist.Add(-1);
+            mylist.Add(true);
+            mylist.Add("chair");
+            Console.WriteLine(String.Join(", ", mylist));
+            //Loop through the list and print all values (Hint: Type 
+            //Inference might help here!)
+            foreach (var o in mylist)
             {
-                array[i] = num + i;
-                Console.WriteLine(array[i]);
+                Console.WriteLine(o);
             }
-        }
-
-        //Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
-        static string[] FourNamesArray()
-        {
-            string[] names = new string[] { "Tim", "Martin", "Nikki", "Sara" };
-            //Console.WriteLine(String.Join(", ", names));
+            //Add all values that are Int type together and output the sum
+            int sum = 0;
+            foreach (var o in mylist)
             {
-                foreach (string person in names)
+                if (o is int)
                 {
-                    Console.WriteLine(person);
+                    sum += (int)o;
                 }
+
             }
-            return names;
+            Console.WriteLine($"Sum is: {sum}");
         }
-
-        static void TenBoolArray()
-        {
-            bool[] arr = new bool[6];
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                arr[i] = true;
-                arr[i + 1] = false;
-                Console.WriteLine(arr[i]);
-                Console.WriteLine(arr[i + 1]);
-            }
-        }
-        /*             
-            ######### List of Flavors ##########
-        */
-        static List<string> Icecream()
-        {
-            //Create a list of ice cream flavors that holds at least 5 different flavors
-            List<string> flavorslist = new List<string>();
-            flavorslist.Add("Flavor1");
-            flavorslist.Add("Flavor2");
-            flavorslist.Add("Flavor3");
-            flavorslist.Add("Flavor4");
-            flavorslist.Add("Flavor5");
-            flavorslist.Add("Flavor6");
-
-            //Console.WriteLine(String.Join(", ", flavorslist));
-            //Output the length of this list after building it
-            //Console.WriteLine(flavorslist.Count);
-            //Output the third flavor in the list, then remove this value
-            //Console.WriteLine(flavorslist[2]);
-            flavorslist.RemoveAt(2);
-            //Console.WriteLine(flavorslist.Count);
-            return flavorslist;
-        }
-
-        public static void userinfo(List<string> flavorslist, string[] names)
-        {
-            //Create a dictionary that will store both string keys as well as string values
-            Dictionary<string, string> user = new Dictionary<string, string>();
-            //Add key/value pairs to this dictionary where:
-            //each key is a name from your names array
-            //each value is a randomly select a flavor from your flavors list.
-            foreach (string name in names)
-            {
-                user.Add(name, null);
-            }
-            Random rand = new Random();
-            foreach (string name in names)
-            {
-                int x = rand.Next(flavorslist.Count - 1);
-                user[name] = flavorslist[x];
-            }
-            //Loop through the dictionary and print out each user's name and their associated ice cream flavor
-            foreach (var entry in user)
-            {
-                Console.WriteLine("{0}'s favorite flavor is {1}!", entry.Key, entry.Value);
-            }
-        }
-
-
     }
 }
---
+++ fundamentals/04-basic13.cs
@@ -0,0 +1,190 @@
+using System;
+
+namespace basic13
+{
+    class Program
+    {
+        public static void PrintNumber()
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void PrintOdds()
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                if ((i % 2) != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void PrintSum()
+        {
+            // Print all of the numbers from 0 to 255, 
+            // but this time, also print the sum as you go. 
+            // For example, your output should be something like this:
+            int sum = 0;
+            for (int i = 1; i <= 255; i++)
+            {
+                sum += i;
+                Console.WriteLine($"numnber:{i}, sum:{sum}");
+            }
+        }
+        public static void LoopArray(int[] numbers)
+        {
+            // Write a function that would iterate through each item of the given integer array and 
+            // print each value to the console.
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int FindMax(int[] numbers)
+        {
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            // Write a function that takes an integer array and prints and returns the maximum value in the array. 
+            // Your program should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]), 
+            // or even a mix of positive numbers, negative numbers and zero.
+            Console.WriteLine($"the largest number in the array is {max}");
+            return max;
+        }
+        public static int GetAverage(int[] numbers)
+        {
+            // Write a function that takes an integer array and prints the AVERAGE of the values in the array.
+            // For example, with an array [2, 10, 3], your program should write 5 to the console.
+            int total = 0;
+            foreach (int number in numbers)
+            {
+                total += number;
+            }
+            int average = total / numbers.Length;
+            Console.WriteLine($"the average in the array is {average}");
+            return average;
+        }
+        public static int GreaterThanY(int[] numbers, int y)
+        {
+            // Write a function that takes an integer array, and a integer "y" and returns the number of array values 
+            // That are greater than the "y" value. 
+            // For example, if array = [1, 3, 5, 7] and y = 3. Your function should return 2 
+            // (since there are two values in the array that are greater than 3).
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number > y)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"there are {count} numbers greater than {y}");
+            return count;
+        }
+
+        public static void SquareArrayValues(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // Write a function that takes an integer array "numbers", and then multiplies each value by itself.
+                // For example, [1,5,10,-10] should become [1,25,100,100]
+                int temp = numbers[i] * numbers[i];
+                numbers[i] = temp;
+            }
+        }
+
+        public static void EliminateNegatives(int[] numbers)
+        {
+            // Given an integer array "numbers", say [1, 5, 10, -2], create a function that replaces any negative number with the value of 0. copy
+            // When the program is done, "numbers" should have no negative values, say [1, 5, 10, 0].
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers[i] = 0;
+                }
+            }
+        }
+        public static void MinMaxAverage(int[] numbers)
+        {
+            // Given an integer array, say [1, 5, 10, -2], create a function that prints the maximum number in the array, 
+            // the minimum value in the array, and the average of the values in the array.
+            int total = 0;
+            int max = numbers[0];
+            int min = numbers[0];
+            foreach (int number in numbers)
+            {
+                total += number;
+                if (number > max) { max = number; }
+                if (number < min) { min = number; }
+            }
+            int average = total / numbers.Length;
+            Console.WriteLine($"the average in the array is {average}");
+            Console.WriteLine($"the max in the array is {max}");
+            Console.WriteLine($"the min in the array is {min}");
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] test = { 1, 5, -6, 4, 0, 7, -9, 6, 6 };
+            LoopArray(test);
+            // FindMax(test);
+            // GetAverage(test);
+            // GreaterThanY(test,0);
+            // EliminateNegatives(test);
+            // // SquareArrayValues(test);
+            // LoopArray(test);
+            // MinMaxAverage(test);
+            // ShiftValues(test);
+            // LoopArray(test);
+            LoopObArray(NumToString(test));
+        }
+        public static void ShiftValues(int[] numbers)
+        {
+            // Given an integer array, say [1, 5, 10, 7, -2], 
+            // Write a function that shifts each number by one to the front and adds '0' to the end. 
+            // For example, when the program is done, if the array [1, 5, 10, 7, -2] is passed to the function, 
+            // it should become [5, 10, 7, -2, 0].
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                numbers[i] = numbers[i + 1];
+            }
+            numbers[numbers.Length - 1] = 0;
+        }
+        public static object[] NumToString(int[] numbers)
+        {
+            // Write a function that takes an integer array and returns an object array 
+            // that replaces any negative number with the string 'Dojo'.
+            // For example, if array "numbers" is initially [-1, -3, 2] 
+            // your function should return an array with values ['Dojo', 'Dojo', 2].
+
+            object[] ob_array = new object[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] >= 0)
+                {
+                    ob_array[i] = numbers[i];
+                }
+                else
+                {
+                    ob_array[i] = "Dojo";
+                }
+            }
+            return ob_array;
+        }
+        public static void LoopObArray(object[] arr)
+        {
+            foreach (object ob in arr)
+            {
+                Console.WriteLine($"{ob}");
+            }
+        }
+    }
+}
